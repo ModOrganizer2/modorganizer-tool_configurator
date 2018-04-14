419cbf34ddf4b0b456ec46ce62d54cbe3c0a713e
AnyOldName3 <krizdjali+github@gmail.com>
2018-04-14 01:16:41 +0100

    Include updated .ts file

--- src/configurator_en.ts
+++ src/configurator_en.ts
@@ -1,6 +1,104 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="en_US">
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>IniEdit</name>
+    <message>
+        <location filename="pyCfg.py" line="412"/>
+        <source>Plugin to allow easier customization of game settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="424"/>
+        <source>Configurator</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="427"/>
+        <source>Modify game Configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="494"/>
+        <source>unexpected section {0} in {1}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="502"/>
+        <source>unknown ini setting {0}</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="507"/>
+        <source>{0} in wrong ini file ({1}, should be {2})</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="524"/>
+        <source>Invalid configuration file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="524"/>
+        <source>Your configuration files contains an invalid value: {0}={1} (in section {2}).
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="524"/>
+        <source>Please note that the game probably won&apos;t report an error, it will just ignore this setting.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="524"/>
+        <source>Please note that even if someone told you to use this setting, that doesn&apos;t mean they know what they&apos;re talking about.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="524"/>
+        <source>BUT, if you know for a fact this is a valid setting, then please contact me at sherb@gmx.net.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="pyCfg.py" line="110"/>
+        <source>Unsaved changes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="110"/>
+        <source>There are unsaved changes. Do you want to save before closing the dialog?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="126"/>
+        <source>Advanced</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="126"/>
+        <source>Basic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="216"/>
+        <source>true</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="216"/>
+        <source>false</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pyCfg.py" line="302"/>
+        <source>Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 <context>
     <name>PyCfgDialog</name>
     <message>

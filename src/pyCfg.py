d8877f329cc4654570e1a8e4a9e09eddb75dc9c4
LostDragonist <lost.dragonist@gmail.com>
2018-12-12 02:01:26 -0600

    Update version to 1.1.0.0

--- src/pyCfg.py
+++ src/pyCfg.py
@@ -391,7 +391,7 @@ class IniEdit(mobase.IPluginTool):
         return self.__tr("Plugin to allow easier customization of game settings")
 
     def version(self):
-        return mobase.VersionInfo(1, 0, 0, mobase.ReleaseType.final)
+        return mobase.VersionInfo(1, 1, 0, 0)
 
     def isActive(self):
         return True
